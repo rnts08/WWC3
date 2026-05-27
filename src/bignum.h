1240812346e507bd840b48908519cd29a02ade09
z <timh.bergstrom@gmail.com>
2026-05-27 12:45:23 +0700

    fix: add bounds check before loop in getuint64/getuint256

--- src/bignum.h
+++ src/bignum.h
@@ -214,6 +214,8 @@ public:
         BN_bn2mpi(this, &vch[0]);
         if (vch.size() > 4)
             vch[4] &= 0x7f;
+        if (vch.size() < 4)
+            return 0;
         uint64_t n = 0;
         for (unsigned int i = 0, j = vch.size()-1; i < sizeof(n) && j >= 4; i++, j--)
             ((unsigned char*)&n)[i] = vch[j];
@@ -284,6 +286,8 @@ public:
         BN_bn2mpi(this, &vch[0]);
         if (vch.size() > 4)
             vch[4] &= 0x7f;
+        if (vch.size() < 4)
+            return 0;
         uint256 n = 0;
         for (unsigned int i = 0, j = vch.size()-1; i < sizeof(n) && j >= 4; i++, j--)
             ((unsigned char*)&n)[i] = vch[j];
